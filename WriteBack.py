59b6eab5cefb594b22dc10236dbaf38163e89ecb
SahdevSa <80705148+SahdevSa@users.noreply.github.com>
2021-04-06 10:33:45 +0530

    Add files via upload

---
+++ WriteBack.py
@@ -0,0 +1,20 @@
+rd = 0x0
+
+Control = 1
+        ########## Assumptions ###########
+# rd : global variable ; detected in decode stage
+# RY is output of stage 4.
+            # control : initalised with 0 #####
+# is a global variable : it should come from decode stage.
+#  it will detect the type of instructon
+# control will become 1 for r-type, i-type, u-type, uj-type
+register = [] # global list 
+for i in range(32):
+    register.append(0)
+
+def WriteBack(RY):
+    if (Control==1):
+        register[rd] = RY
+
+WriteBack(0x00000008)
+print(register)
