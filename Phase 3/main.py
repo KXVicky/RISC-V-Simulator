b7d335801fe00167b326db749973533bdd31ba1d
SahdevSa <80705148+SahdevSa@users.noreply.github.com>
2021-05-17 08:32:51 +0530

    Update main.py

--- Phase 3/main.py
+++ Phase 3/main.py
@@ -1,5 +1,5 @@
 from bitstring import BitArray
-
+import math
 IR = ''
 pc = 0x0
 pc_temp = 0x0
@@ -62,6 +62,156 @@ DataMemory = dict.fromkeys(range(0x10000000, 0x10000194, 1), 0)
 StoreInst = ["sb", "sw", "sh", "sd"]
 loadInst = ["lb", "lw", "ld", "lh", "lui"]
 
+
+
+bo_bits = 0
+index_bits = 0
+tag_bits = 0
+data_copy = 0
+cachesize = 0
+blocksize = 0
+n = 0
+SA = {}
+InsNumHits = 0
+InsNUmMisses = 0
+
+Bo = 0                  # Block offset
+ind = 0                 # index bits
+tag = 0                 # tags
+data = 0
+pc = 0x0
+
+cachesize=int(input("Enter cache size(in bytes without unit):"))
+blocksize=int(input("Enter cache block size(in bytes without unit):"))
+n=int(input("Enter number of ways for Set Associative:"))
+blocks=cachesize/blocksize
+numsets=blocks/n
+
+
+################################# Instruction Cache part ##########
+def inst_fetch(lines):
+    global pc, data
+    global bo_bits, index_bits, tag_bits, data_copy, cachesize, blocksize, n, SA
+    array = []
+    for i in lines:
+        array.append(i.split())
+        
+    MCode = ''
+    for i in array:
+        for j in range(len(i)):
+            if i[j] == str(hex(pc)):
+                MCode = i[j+1]  
+
+    data_copy = pc
+    data = int(MCode, 16)
+
+    # instruction address sent from BTB or IAG
+    # number of bits in instruction adddress
+    add_bits=32                           # 32 for risc-v 32 bit ISA
+    bo_bits=int(math.log2(blocksize))     # number of bits in block offset
+    index_bits=int(math.log2(numsets))    # number of bits in index
+    tag_bits=add_bits-bo_bits-index_bits  # number of bits in tag
+
+    Size = int(blocks)//int(n)
+    M = [ [0, 0] for j in range(Size) ]
+    SA = {i:M for i in range(int(n))}
+    pc+=0x4
+
+
+def indexAndTag(data_copy):
+    global ind, tag
+    if index_bits == 0:
+        ind = 0
+    elif index_bits == 1:
+        ind = data_copy & 1
+        tag = data_copy>>1
+    elif index_bits == 2:
+        ind = data_copy & 3
+        tag = data_copy>>2
+    elif index_bits == 3:
+        ind = data_copy & 7
+        tag = data_copy>>3
+    elif index_bits == 4:
+        ind = data_copy & 15
+        tag = data_copy>>4
+    elif index_bits == 5:
+        ind = data_copy & 31
+        tag = data_copy>>5
+    elif index_bits == 6:
+        ind = data_copy & 63
+        tag = data_copy>>6
+    elif index_bits == 7:
+        ind = data_copy & 127
+        tag = data_copy>>7
+    elif index_bits == 8:
+        ind = data_copy & 255
+        tag = data_copy>>8
+    elif index_bits == 9:
+        ind = data_copy & 511
+        tag = data_copy>>9
+    elif index_bits == 10:
+        ind = data_copy & 1023
+        tag = data_copy>>10
+
+
+def BlockOffSet(data_copy):
+    global Bo
+    if bo_bits == 0:
+        Bo = 0
+    elif bo_bits == 1:
+        Bo = data_copy & 1
+        data_copy = data_copy>>1
+    elif bo_bits == 2:
+        Bo = data_copy & 3
+        data_copy = data_copy>>2
+    elif bo_bits == 3:
+        Bo = data_copy & 7
+        data_copy = data_copy>>3
+    elif bo_bits == 4:
+        Bo = data_copy & 15
+        data_copy = data_copy>>4
+    elif bo_bits == 5:
+        Bo = data_copy & 31
+        data_copy = data_copy>>5
+    elif bo_bits == 6:
+        Bo = data_copy & 63
+        data_copy = data_copy>>6
+    elif bo_bits == 7:
+        Bo = data_copy & 127
+        data_copy = data_copy>>7
+    elif bo_bits == 8:
+        Bo = data_copy & 255
+        data_copy = data_copy>>8
+    elif bo_bits == 9:
+        Bo = data_copy & 511
+        data_copy = data_copy>>9
+    elif bo_bits == 10:
+        Bo = data_copy & 1023
+        data_copy = data_copy>>10
+    
+    indexAndTag(data_copy)
+
+
+def UpdateSAInst():
+    BlockOffSet(data_copy)
+    global SA, ind, InsNUmMisses, InsNumHits
+    predict = ""
+    for way in SA:
+        for index in range(len(SA[way])):
+            if index == ind:
+                if SA[way][index][0] == tag:
+                    predict = "hit"
+                    InsNumHits += 1
+                    return SA[way][index][1]
+                else:
+                    predict = "miss"
+                    InsNUmMisses += 1
+                    SA[way][index][0] = tag
+                    if 0x10000000<pc:
+                        SA[way][index][1] = data
+                    else:
+                        SA[way][index][1] = data
+
 def Fetch(lines):
     global pc
     
@@ -108,6 +258,7 @@ def pipelineFetch(lines):
         print("Branch or Jump Instruction")
     return MCode
 
+####################################################################################
 
 # code by ashish sulania
 # assuming RA,RB,rd and imme as global variable and register as a global list 
@@ -1507,6 +1658,39 @@ def initMemory():
 					
     file.close()
 	
+######################## EDING ######
+
+def Data_cache(address):        # Address will come from ALU
+    global bo_bits, index_bits, tag_bits, data_copy, cachesize, blocksize, n, SA
+    data_copy = address
+
+    # instruction address sent from BTB or IAG
+    # number of bits in instruction adddress
+
+    add_bits=32                           # 32 for risc-v 32 bit ISA
+    bo_bits=int(math.log2(blocksize))     # number of bits in block offset
+    index_bits=int(math.log2(numsets))    # number of bits in index
+    tag_bits=add_bits-bo_bits-index_bits  # number of bits in tag
+
+def UpdateSAData():
+    global SA, ind, DataMemory, TextMemory, DataNumHits, DataNumMisses
+    predict = ""
+    for way in SA:
+        for index in range(len(SA[way])):
+            if index == ind:
+                if SA[way][index][0] == tag:
+                    predict = "hit"
+                    DataNumHits += 1
+                    return SA[way][index][1]
+                else:
+                    predict = "miss"
+                    DataNumMisses += 1
+                    SA[way][index][0] = tag
+                    if 0x0FFFFFE8<=address:
+                        SA[way][index][1] = DataMemory[address]
+                    else:
+                        SA[way][index][1] = TextMemory[address]
+
 def pipelineMemory_access(operation, lines):
 	global DataMemory, RY, multiClock
 	multiClock = multiClock + 1
@@ -1531,6 +1715,8 @@ def Memory_access(operation, lines):
     elif operation in StoreInst:
         DataMemory[aluRZ] = RA
     print("DataMemory: ", DataMemory)
+
+#########################################################
 
 def WriteBack():
     global register, multiClock
@@ -1563,6 +1749,30 @@ def Pipeline():
             WriteBack(); pipelineMemory_access(Operation, Operation); pipelineAlu(Operation); Operation = pipelinedecode(int(IR, 16)); IR = pipelineFetch(lines)
         else:
             WriteBack(); pipelineMemory_access(Operation, Operation); pipelineAlu(Operation); Operation = pipelinedecode(IR); IR = pipelineFetch(lines)
+
+def CachePipeline():
+    global clock, Control, cap
+    global memory
+    file = open("instruction.mc","r")
+    lines =file.readlines()
+    n = len(lines)
+    IR = inst_fetch(lines); IR = UpdateSAInst(lines)
+    lines = lines[1:]
+
+    IR = int(IR, 16)
+    Operation = pipelinedecode(IR); inst_fetch(lines); IR = UpdateSAInst(lines)
+
+    pipelineAlu(Operation); Operation = pipelinedecode(int(IR, 16)); inst_fetch(lines); IR = UpdateSAInst(lines)
+
+    if IR!='':
+        Data_cache(RY); UpdateSAData(); pipelineAlu(Operation); Operation = pipelinedecode(int(IR, 16)); inst_fetch(lines); IR = UpdateSAInst(lines)
+    else:
+        Data_cache(RY); UpdateSAData(); pipelineAlu(Operation); Operation = pipelinedecode(IR); inst_fetch(lines); IR = UpdateSAInst(lines)
+    for i in range(n):
+        if IR!='':
+            WriteBack(); Data_cache(RY); UpdateSAData(); pipelineAlu(Operation); Operation = pipelinedecode(int(IR, 16)); inst_fetch(lines); IR = UpdateSAInst(lines)
+        else:
+            WriteBack(); Data_cache(RY); UpdateSAData(); pipelineAlu(Operation); Operation = pipelinedecode(IR); inst_fetch(lines); IR = UpdateSAInst(lines)
             
     file.close()
 
@@ -1610,7 +1820,7 @@ def main():
         knobSpecial.lower()
         if(knobSpecial == 'on'):
             specialInst = int(input('Enter the Instruction number (in decimal base) : '))
-        Pipeline()
+        CachePipeline()
         
         print('Total Cycles : ', multiClock)
         print('Total Instructions : ', instcount)
@@ -1624,6 +1834,19 @@ def main():
         print('No. of branch mispredictions : ', branchMispridiction)
         print('No. of data Hazard stall : ', stallDataHazard)
         print('No. of control Hazard stall : ', stallControlHazard)
+
+        print("\n")
+        print("\nData Cache: ")
+        print("Number of accesess:_____ ", InsNUmMisses+InsNumHits)
+        print("Number of Hits:_____ ", InsNumHits)
+        print("Number of misses:_____ ", InsNUmMisses)
+        print("\n")
+
+        print("\nData Cache: ")
+        print("Number of accesess:_____ ", DataNumMisses+DataNumHits)
+        print("Number of Hits:_____ ", DataNumHits)
+        print("Number of misses:_____ ", DataNumMisses)
+        print("\n")
 
     elif(knobPipeline == 'off'):
         nonPipeline()
